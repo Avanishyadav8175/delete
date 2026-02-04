76500ac1ad39a685bfeb7c3c343dadb25bef3a79
Avanish Yadav <152910693+Avanishyadav8175@users.noreply.github.com>
2026-02-04 18:26:31 +0530

    server folder fixed

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config(); // only once at the top
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const uniqueCodeRoutes = require("./routes/uniqueCodeRoutes");
+
+const app = express();
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "http://localhost:5174",
+  "http://localhost:5174",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.use("/api", userRoutes);
+app.use("/api/unique-code", uniqueCodeRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
